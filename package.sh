6c0e4a135c257be9bdd902fc26f2485d49fc792d
hugo <hugo@illconditioned.com>
2019-11-11 15:34:44 -0800

    try tags again

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@ export CONDA_OUTPUT=/tmp/output
 rm -rf ${CONDA_OUTPUT}
 mkdir ${CONDA_OUTPUT}
 conda build --output-folder=$CONDA_OUTPUT --prefix-length 80 -c conda-forge -c saturncloud conda.recipe
-export TARGET=`ls ${CONDA_OUTPUT}noarch/sutils*.bz2`
+export TARGET=`ls ${CONDA_OUTPUT}/noarch/sutils*.bz2`
 anaconda -t ${ANACONDA_TOKEN} upload -u saturncloud -l dev -l main --force --no-progress
