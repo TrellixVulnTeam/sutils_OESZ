d37cea1ff5c9668446816c7a9f6e3e477f86c1e9
hugo <hugo@illconditioned.com>
2019-11-11 15:43:17 -0800

    fix package

--- package.sh
+++ package.sh
@@ -3,4 +3,4 @@ rm -rf ${CONDA_OUTPUT}
 mkdir ${CONDA_OUTPUT}
 conda build --output-folder=$CONDA_OUTPUT --prefix-length 80 -c conda-forge -c saturncloud conda.recipe
 export TARGET=`ls ${CONDA_OUTPUT}/noarch/sutils*.bz2`
-anaconda -t ${ANACONDA_TOKEN} upload -u saturncloud -l dev -l main --force --no-progress
+anaconda -t ${ANACONDA_TOKEN} upload -u saturncloud ${TARGET} -l dev -l main --force --no-progress
